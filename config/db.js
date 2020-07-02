542f410322a14c9156e6a7970a2444d48b377424
Ogbonna Vitalis <agavitalisogbonna@gmail.com>
2020-07-02 01:43:51 +0100

    Updated my portfolio

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose')
 
 //lets conneect to our mongodb database for local
-mongoose.connect('mongodb://localhost:27017/portfolio',{useUnifiedTopology: true,useNewUrlParser: true});
+//mongoose.connect('mongodb://localhost:27017/portfolio',{useUnifiedTopology: true,useNewUrlParser: true});
 
 //before going live, uncomment this connection
-//mongoose.connect('mongodb://portfolio:Chidiebere94@ds131784.mlab.com:31784/portfolio', {useUnifiedTopology: true,useNewUrlParser: true});
+mongoose.connect('mongodb://portfolio:Chidiebere94@ds131784.mlab.com:31784/portfolio', {useUnifiedTopology: true,useNewUrlParser: true});
 
 //lets check if the connection was not successful
 mongoose.connection.on('error', console.error.bind(console, 'connection error'))
